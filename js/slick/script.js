0c46636a700b3082ddaac49037de392860f802b0
Lepor0103 <103307097+Lepor0103@users.noreply.github.com>
2022-04-17 18:06:50 +0500

    Add files via upload

---
+++ js/slick/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.tips-slider').slick({
+        arrows:true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        prevArrow: '<button type="button" class="slick-arrow_prev slick-arrow"></button>',
+        nextArrow: '<button type="button" class="slick-arrow_next slick-arrow"></button>',
+    });
+    
+    
+});
